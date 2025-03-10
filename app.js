a95fde6238a033c71bd05a164687a30971bce3cd
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2025-03-10 13:59:11 -0300

    Handling DELETE Requests

--- app.js
+++ app.js
@@ -76,6 +76,23 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
