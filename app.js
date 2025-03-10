d8f2dab87d1116a2c2360c0469920ebdb4c4e23b
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2025-03-10 13:03:06 -0300

    Handling PATCH Requests

--- app.js
+++ app.js
@@ -57,6 +57,25 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
