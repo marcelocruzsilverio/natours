6d04cc0d8ec9489795e6c4bf98acf04357738bf5
marcelocruzsilverio <marcelosilveriocruz@gmail.com>
2025-03-17 18:31:21 -0300

    Deleting Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -73,12 +73,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  const id = req.params.id * 1;
-  //const tour = tours.find((el) => el.id === id);
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
